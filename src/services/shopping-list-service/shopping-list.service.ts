aa617301b3079a952fec1f739164ccaafca4419c
Arun Antony <arun.antony@knab.nl>
2018-11-21 13:36:02 +0100

    need to change the type from Observer to any on shoppingListService for the subscribe to work

--- src/services/shopping-list-service/shopping-list.service.ts
+++ src/services/shopping-list-service/shopping-list.service.ts
@@ -29,7 +29,7 @@ export class ShoppingListService {
         }
     }
 
-    addNewIngredientListener (calBkFn: Observer<Function>) {
+    addNewIngredientListener (calBkFn: any) {
         this.addNewIngredientEvent.subscribe(calBkFn);
     }
     
